d70d35fd1442c1cd8a357080061ca6c9c6d177db
Artur Jamro <artur.jamro@gmail.com>
2019-11-30 00:09:29 +0100

    More progress at assigment #3

--- zad3-calc/calc.v
+++ zad3-calc/calc.v
@@ -9,9 +9,30 @@ module calc(
     output wire [7:0] led
     );
 
+localparam BTN_PUSH   = 4'b0010;
+localparam BTN_EXTEND = 4'b0100;
+localparam BTN_EXEC   = 4'b1000;
+localparam BTN_RESET  = 4'b1001;
+localparam CMD_ADD = 3'b000;
+localparam CMD_SUB = 3'b001;
+localparam CMD_MUL = 3'b010;
+localparam CMD_DIV = 3'b011;
+localparam CMD_MOD = 3'b100;
+localparam CMD_POP = 3'b101;
+localparam CMD_DUP = 3'b110;
+localparam CMD_SWP = 3'b111;
+
+localparam MAX_ELEM_CNT = 512;
+
 localparam STATE_IDLE = 0;
 localparam STATE_CALC = 1;
-localparam STATE_MEM_IO = 2; // todo (disable stack op)
+localparam STATE_MEM_WAIT = 2;
+localparam STATE_MEM_WAIT2 = 3;
+localparam STATE_WAIT_NO_INPUT = 4;
+
+localparam ST_NO_MOV = 0;
+localparam ST_MOV_UP = 1;
+localparam ST_MOV_DN = 2;
 
 wire [3:0] btn;
 wire [7:0] sw;
@@ -19,9 +40,10 @@ sync_input #(.BITS(4)) btn_sync(.clk(clk), .in(btn_raw), .out(btn));
 sync_input #(.BITS(8)) sw_sync(.clk(clk), .in(sw_raw), .out(sw));
 
 reg err_flag;
-reg [1:0] state;
+reg [2:0] state;
 
 reg st_en;
+reg st_reset;
 reg [1:0] st_write_elems_cnt;
 reg [31:0] st_write_elems [0:1];
 reg [1:0] st_top_mov;
@@ -32,6 +54,7 @@ wire st_ready;
 stack stack_mem(
     .clk(clk),
     .en(st_en),
+    .reset(st_reset),
     .write_elems_cnt(st_write_elems_cnt),
     .write_elem0(st_write_elems[0]),
     .write_elem1(st_write_elems[1]),
@@ -59,12 +82,76 @@ assign led = {err_flag, st_elems_cnt[6:0]};
 
 // todo the whole logic...
 always @(posedge clk) begin
+    st_en <= 0;
+    st_reset <= 0;
     case(state)
     STATE_IDLE: begin
+        if (btn != 0) begin
+            state <= STATE_WAIT_NO_INPUT; // might get overwritten
+        end
+        case(btn)
+        BTN_PUSH: begin
+            if (st_elems_cnt == MAX_ELEM_CNT) begin
+                err_flag <= 1;
+            end else begin
+                st_en <= 1;
+                st_write_elems_cnt <= 1;
+                st_write_elems[0] <= {24'h0, sw[7:0]};
+                st_top_mov <= ST_MOV_UP;
+                state <= STATE_MEM_WAIT;
+            end
+        end
+        BTN_EXTEND: begin
+            st_en <= 1;
+            st_write_elems_cnt <= 1;
+            st_write_elems[0] <= {st_top[0][23:0], sw[7:0]};
+            st_top_mov <= ST_NO_MOV;
+            state <= STATE_MEM_WAIT;
+        end
+        BTN_EXEC: begin
+            // todo
+            case(sw[2:0])
+            CMD_ADD: begin
+            end
+            CMD_SUB: begin
+            end
+            CMD_MUL: begin
+            end
+            CMD_DIV: begin
+            end
+            CMD_MOD: begin
+            end
+            CMD_POP: begin
+            end
+            CMD_DUP: begin
+            end
+            CMD_SWP: begin
+            end
+            endcase
+        end
+        BTN_RESET: begin
+            st_reset <= 1;
+            err_flag <= 0;
+            state <= STATE_MEM_WAIT;
+        end
+        endcase
     end
     STATE_CALC: begin
+        // doing calc? (todo)
     end
-    STATE_MEM_IO: begin
+    STATE_MEM_WAIT: begin
+        // memory works now and sets st_ready in next tick
+        state <= STATE_MEM_WAIT2;
+    end
+    STATE_MEM_WAIT2: begin
+        if (st_ready) begin
+            state <= STATE_WAIT_NO_INPUT;
+        end
+    end
+    STATE_WAIT_NO_INPUT: begin
+        if (btn == 0 || btn == BTN_RESET) begin
+            state <= STATE_IDLE;
+        end
     end
     endcase
 end
@@ -78,6 +165,7 @@ endmodule
 module stack(
     input wire clk,
     input wire en,
+    input wire reset,
     input wire [1:0] write_elems_cnt, // 0, 1 or 2 (counting from top)
     input wire [31:0] write_elem0,
     input wire [31:0] write_elem1,
@@ -93,9 +181,9 @@ localparam ST_MOV_UP = 1;
 localparam ST_MOV_DN = 2;
 
 reg [31:0] mem [0:511];
-
-reg [31:0] extra_elem;
-reg extra_elem_present;
+reg [9:0] tmp_write_idx;
+reg [31:0] tmp_write_elem;
+reg tmp_do_write;
 
 initial begin
     elems_cnt <= 0;
@@ -103,37 +191,61 @@ initial begin
 end
 
 always @(posedge clk) begin
-    if (extra_elem_present) begin // two writes only if swapping two top elements
-        // todo: probably refactor it
-        mem[elems_cnt - 1] <= extra_elem;
-        top1 <= extra_elem;
-        ready <= 1;
-    end else if (en) begin
-        // todo
-    end
+    ready <= 1; // might be changed
+    if (reset) begin
+        elems_cnt <= 0;
+    end else begin
+        tmp_do_write = 0;
+        if (!ready) begin // two writes only if swapping two top elements
+            // mem[elems_cnt - 2] <= top1;
+            tmp_do_write = 1;
+            tmp_write_idx = elems_cnt - 2;
+            tmp_write_elem = top1;
+        end else if (en) begin
+            case(top_mov)
+            ST_NO_MOV: begin
+                if (write_elems_cnt >= 1) begin // should be always
+                    // mem[elems_cnt - 1] <= write_elem0;
+                    tmp_do_write = 1;
+                    tmp_write_idx = elems_cnt - 1;
+                    tmp_write_elem = write_elem0;
+                    top0 <= write_elem0;
+                end
+                if (write_elems_cnt == 2) begin
+                    ready <= 0;
+                    top1 <= write_elem1;
+                end
+            end
+            ST_MOV_UP: begin
+                // always writing exactly one element
+                elems_cnt <= elems_cnt + 1;
+                // mem[elems_cnt] <= write_elem0;
+                tmp_do_write = 1;
+                tmp_write_idx = elems_cnt;
+                tmp_write_elem = write_elem0;
+                top0 <= write_elem0;
+                top1 <= top0;
+            end
+            ST_MOV_DN: begin
+                elems_cnt <= elems_cnt - 1;
+                if (write_elems_cnt > 0) begin // cnt == 1
+                    // mem[elems_cnt - 2] <= write_elem0;
+                    tmp_do_write = 1;
+                    tmp_write_idx = elems_cnt - 2;
+                    tmp_write_elem = write_elem0;
+                    top0 <= write_elem0;
+                end else begin
+                    top0 <= top1;
+                end
+                top1 <= mem[elems_cnt - 3];
+            end
+            endcase
+        end
 
-    // mem_io_write_idx <= mem_io_write_idx - 1;
-    // stack[elems_cnt - 1 - mem_io_write_idx] <= mem_io_write_buf[mem_io_write_idx];
-    // if (mem_io_write_idx) begin
-    //     top1_num <= mem_io_write_buf[1]; // only for swapping two top elems
-    // end else begin
-    //     state <= STATE_IDLE;
-    //     case (mem_io_mov)
-    //     MEM_IO_NO_MOV: begin
-    //         top0_num <= mem_io_write_buf[0];
-    //     end
-    //     MEM_IO_MOV_UP: begin
-    //         top0_num <= mem_io_write_buf[0];
-    //         top1_num <= top0_num;
-    //         top2_num <= top1_num;
-    //     end
-    //     MEM_IO_MOV_DN: begin
-    //         top0_num <= top1_num;
-    //         top1_num <= top2_num;
-    //         top2_num <= stack[elems_cnt - 3];
-    //     end
-    //     endcase
-    // end
+        if (tmp_do_write) begin
+            mem[tmp_write_idx] <= tmp_write_elem;
+        end
+    end
 end
 
 endmodule
